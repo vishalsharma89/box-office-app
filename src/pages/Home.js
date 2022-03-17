21e8f07154980a21c8a43235117c4ed3fd67e56d
vishalsharma89 <sharmavishal9643@gmail.com>
2022-03-18 00:59:15 +0530

    displayed show results and fetch api used

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
-
+import { apiGet } from '../misc/config';
 const Home = () => {
     const [input, setInput] = useState('');
+    const [results, setResults] = useState(null);
     const onSearch = () => {
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r => r.json()).then(result => {
+        apiGet(`/search/shows?q=${input}`).then(result => {
+            setResults(result);
             console.log(result);
         })
     }
@@ -18,10 +20,29 @@ const Home = () => {
             onSearch()
         }
     }
+    const renderResults = () => {
+        if (results && results.length === 0) {
+            return <div>No Results</div>;
+
+        }
+        if (results && results.length > 0) {
+            return (
+                <div>
+                    {results.map((item) => (
+                        <div key={item.show.id}>{item.show.name}</div>
+                    ))}
+                </div>
+            )
+        }
+
+        return null;
+
+    }
     return (
         <MainPageLayout>
             <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input} />
             <button type='button' onClick={onSearch} >Search</button>
+            {renderResults()}
         </MainPageLayout>
     )
 }
